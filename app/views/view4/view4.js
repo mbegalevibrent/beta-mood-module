091217fc8c71bb6252947bb184987d7e41a7e6bb
Abhi <abhishek.vit@gmail.com>
2017-12-03 17:19:26 -0800

    PHQ-9 content (needs to be modified for prod)

--- app/views/view4/view4.js
+++ app/views/view4/view4.js
@@ -53,7 +53,9 @@ angular.module('myApp.view4', ['ngRoute'])
 
     $scope.onboardingContent = [];
     // Abhi to comment 
-    $scope.onboardingContent['phq'] = {text:'PHQ intro content', image:'phq8.png'};
+    PHQ9_INTRO = '<p><b> placeholder PHQ-9: a depression screening tool</b><br/>The Patient Health Questionnaire, or PHQ-9, is a clinically validated 9-question screening tool used widely by doctors and health professionals. It takes about 5 minutes to answer the questions and get results.</p>'
+
+    $scope.onboardingContent['phq'] = {text: PHQ9_INTRO, image:'phq8.png'};
     $scope.onboardingContent['lss'] = {text:'Life Satisfaction Survey intro content', image:'lss.png'};
     $scope.onboardingContent['panas'] = {text:'We are going to ask you to describe different feelings and emotions. On the next few pages, choose the word that best represents how you are feeling right now! ', image:'panas.png'};
 
